4994918a8f4685382826dce9ecef661474844fc8
Javier Vega <jave@microsoft.com>
2024-04-29 11:25:14 -0700

    adding other components

--- cvm-attestation/tests/test_measurements.py
+++ cvm-attestation/tests/test_measurements.py
@@ -1,7 +1,15 @@
-import pytest
-import winreg
-from src.measurements import get_measurements
+# test_Measurements.py
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 
+import pytest
+import sys
+from src.measurements import get_measurements
+# Check the platform
+if sys.platform.startswith('win32'):
+    # Import modules specific to Windows
+    import winreg
 
 # Define the registry path
 PATH = r'SYSTEM\\CurrentControlSet\\Control\\IntegrityServices'
@@ -13,7 +21,7 @@ def mock_winreg(mocker):
     mocker.patch.object(winreg, 'OpenKey')
 
 
-# Test case for the get_measurements function
+@pytest.mark.skipif(sys.platform != "windows", reason="only runs on windows")
 def test_get_measurements():
     winreg.OpenKey.assert_called_with(winreg.HKEY_LOCAL_MACHINE, PATH, 0, winreg.KEY_READ)
     # Expected value from the registry
