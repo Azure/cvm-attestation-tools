3fa712f4830adb70ce996518087fe5d9f1a4ca7a
Javier Vega <jave@microsoft.com>
2024-04-30 09:56:15 -0700

    adding retry logic for requests

--- cvm-attestation/src/ImdsClient.py
+++ cvm-attestation/src/ImdsClient.py
@@ -5,6 +5,7 @@
 
 import requests
 import json
+import time
 from src.Logger import Logger
 
 # IMDS endpoint for getting the TD Quote
@@ -37,30 +38,47 @@ class ImdsClient:
       "report": encoded_report
     }
 
-    try:
-      self.log.info("Starting td quote request")
-      response = requests.post(
+    # Request TD Quote with 5 retries delaying by 1 second each request
+    delay = 1
+    max_retries = 5
+    retries = 0
+    while retries < max_retries:
+      try:
+        self.log.info("Starting td quote request")
+        response = requests.post(
           IMDS_ENDPOINT,
           data=json.dumps(request_body),
           headers=headers
-      )
+        )
 
-      if response.status_code == 200:
+        if response.status_code == 200:
           self.log.info("Received td quote successfully")
           evidence_json = json.loads(response.text)
           encoded_quote = evidence_json['quote']
 
           return encoded_quote
-      else:
+        else:
           self.log.error('Failed to get td quote')
           self.log.error(f'response: {response.text}')
-          raise TDQuoteException(f'Error {response.status_code}: {response.text}')
-    except requests.exceptions.RequestException as e:
-      self.log.error("Exception while fetching td quote", exc_info=True)
-      raise TDQuoteException('HTTP request failed') from e
-    except json.JSONDecodeError as e:
-      self.log.error("JSON decode error", exc_info=True)
-      raise TDQuoteException('Error decoding JSON response') from e
+
+          retries += 1
+          if retries >= max_retries:
+            raise TDQuoteException(f'Error {response.status_code}: {response.text}')
+
+          self.log.info(f"Retrying in {delay} seconds... Attempt {retries}")
+          time.sleep(delay)
+      except requests.exceptions.RequestException as e:
+        self.log.error("Exception while fetching td quote", exc_info=True)
+
+        retries += 1
+        if retries >= max_retries:
+          raise TDQuoteException(f'HTTP request failed with error {e}') from e
+
+        self.log.info(f"Retrying in {delay} seconds... Attempt {retries}")
+        time.sleep(delay)
+      except json.JSONDecodeError as e:
+        self.log.error("JSON decode error", exc_info=True)
+        raise TDQuoteException(f'Error decoding JSON response. Error: {e}') from e
 
 
   def get_vcek_certificate(self):
@@ -70,28 +88,47 @@ class ImdsClient:
       'Metadata': 'true'
     }
 
-    try:
-      self.log.info("Starting vcek certificate request")
-      response = requests.get(
-        THIM_ENDPOINT,
-        headers = headers)
+    # Request Vcek Certificate with 5 retries delaying by 1 second each request
+    delay = 1
+    max_retries = 5
+    retries = 0
+    while retries < max_retries:
+      try:
+        self.log.info("Starting vcek certificate request")
+        response = requests.get(
+          THIM_ENDPOINT,
+          headers = headers
+        )
 
-      if response.status_code == 200:
-        self.log.info("Received certificate successfully")
-        data_json = json.loads(response.text)
-        cert = data_json['vcekCert']
-        chain = data_json['certificateChain']
-        cert_chain = cert + chain
-        cert_chain = bytearray(cert_chain.encode('utf-8'))
+        if response.status_code == 200:
+          self.log.info("Received certificate successfully")
+          data_json = json.loads(response.text)
+          cert = data_json['vcekCert']
+          chain = data_json['certificateChain']
+          cert_chain = cert + chain
+          cert_chain = bytearray(cert_chain.encode('utf-8'))
 
-        return cert_chain
-      else:
+          return cert_chain
+        else:
           self.log.error('Failed to get vcek certificate')
           self.log.error(f'response: {response.text}')
-          raise VcekCertException(f'Error {response.status_code}: {response.text}')
-    except requests.exceptions.RequestException as e:
-      self.log.error("Exception while fetching vcek certificate", exc_info=True)
-      raise VcekCertException('HTTP request failed') from e
-    except json.JSONDecodeError as e:
-      self.log.error("JSON decode error", exc_info=True)
-      raise VcekCertException('Error decoding JSON response') from e
+
+          retries += 1
+          if retries >= max_retries:
+            raise VcekCertException(f'Error {response.status_code}: {response.text}')
+
+          self.log.info(f"Retrying in {delay} seconds... Attempt {retries}")
+          time.sleep(delay)
+      except requests.exceptions.RequestException as e:
+        self.log.error("Exception while fetching vcek certificate", exc_info=True)
+
+        retries += 1
+        if retries >= max_retries:
+          raise VcekCertException(f'HTTP request failed with error {e}') from e
+
+        self.log.info(f"Retrying in {delay} seconds... Attempt {retries}")
+        time.sleep(delay)
+      except json.JSONDecodeError as e:
+        print(e)
+        self.log.error("JSON decode error", exc_info=True)
+        raise VcekCertException(f'Error decoding JSON response. Error: {e}') from e
--- cvm-attestation/tests/test_ImdsClient.py
+++ cvm-attestation/tests/test_ImdsClient.py
@@ -47,15 +47,68 @@ def test_successful_vcek_cert_request(mocker, imds_client):
     assert bytes('some_cert', 'utf-8') in evidence
 
 
+def test_td_quote_request_retries_if_error_code_is_returned(mocker, imds_client):
+    mock_response = MagicMock()
+    mock_response.status_code = 400
+    mock_response.text = 'Error message'
+
+
+    with patch('time.sleep', return_value=None) as mock_sleep:
+        with pytest.raises(TDQuoteException) as excinfo:
+            with patch('requests.post', return_value=mock_response) as mock_post:
+                imds_client.get_td_quote('some_encoded_report')
+            assert mock_post.call_count == 5
+        assert 'Error 400: Error message' in str(excinfo.value)
+        mock_sleep.assert_called_with(1)
+        assert mock_sleep.call_count == 4
+
+
 def test_td_quote_request_fails_with_http_400(mocker, imds_client):
     mock_response = MagicMock()
     mock_response.status_code = 400
     mock_response.text = 'Error message'
     mocker.patch('requests.post', return_value=mock_response)
 
-    with pytest.raises(TDQuoteException) as excinfo:
-        imds_client.get_td_quote('some_encoded_report')
-    assert 'Error 400: Error message' in str(excinfo.value)
+
+    with patch('time.sleep', return_value=None) as mock_sleep:
+        with pytest.raises(TDQuoteException) as excinfo:
+            imds_client.get_td_quote('some_encoded_report')
+        assert 'Error 400: Error message' in str(excinfo.value)
+        mock_sleep.assert_called_with(1)
+        assert mock_sleep.call_count == 4
+
+
+def test_td_quote_request_retries_if_http_exception_is_encountered(mocker, imds_client):
+    mock_response = MagicMock()
+    mock_response.status_code = 400
+    mock_response.text = 'Error message'
+    effect = requests.exceptions.RequestException("some_error")
+
+
+    with patch('time.sleep', return_value=None) as mock_sleep:
+        with patch('requests.post',side_effect=effect, return_value=mock_response) as mock_post:
+            with pytest.raises(TDQuoteException) as excinfo:
+                imds_client.get_td_quote('some_encoded_report')
+            assert 'HTTP request failed with error some_error' in str(excinfo.value)
+        assert mock_post.call_count == 5
+        mock_sleep.assert_called_with(1)
+        assert mock_sleep.call_count == 4
+
+
+def test_vcek_cert_request_retries_if_error_code_is_returned(mocker, imds_client):
+    mock_response = MagicMock()
+    mock_response.status_code = 400
+    mock_response.text = 'Error message'
+
+
+    with patch('time.sleep', return_value=None) as mock_sleep:
+        with pytest.raises(VcekCertException) as excinfo:
+            with patch('requests.get', return_value=mock_response) as mock_post:
+                imds_client.get_vcek_certificate()
+            assert mock_post.call_count == 5
+        assert 'Error 400: Error message' in str(excinfo.value)
+        mock_sleep.assert_called_with(1)
+        assert mock_sleep.call_count == 4
 
 
 def test_vcek_cert_request_fails_with_http_400(mocker, imds_client):
@@ -64,6 +117,49 @@ def test_vcek_cert_request_fails_with_http_400(mocker, imds_client):
     mock_response.text = 'Error message'
     mocker.patch('requests.get', return_value=mock_response)
 
-    with pytest.raises(VcekCertException) as excinfo:
-        imds_client.get_vcek_certificate()
-    assert 'Error 400: Error message' in str(excinfo.value)
+    with patch('time.sleep', return_value=None) as mock_sleep:
+        with pytest.raises(VcekCertException) as excinfo:
+            imds_client.get_vcek_certificate()
+        assert 'Error 400: Error message' in str(excinfo.value)
+        mock_sleep.assert_called_with(1)
+        assert mock_sleep.call_count == 4
+
+
+def test_vcek_cert_request_retries_if_http_exception_is_encountered(mocker, imds_client):
+    mock_response = MagicMock()
+    mock_response.status_code = 400
+    mock_response.text = 'Error message'
+    effect = requests.exceptions.RequestException("some_error")
+
+
+    with patch('time.sleep', return_value=None) as mock_sleep:
+        with patch('requests.get',side_effect=effect, return_value=mock_response) as mock_post:
+            with pytest.raises(VcekCertException) as excinfo:
+                imds_client.get_vcek_certificate()
+            assert 'HTTP request failed with error some_error' in str(excinfo.value)
+        assert mock_post.call_count == 5
+        mock_sleep.assert_called_with(1)
+        assert mock_sleep.call_count == 4
+
+def test_td_quote_fails_to_decode_json_response(mocker, imds_client):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.text = 'invalid_json'
+
+    with patch('requests.post', return_value=mock_response) as mock_post:
+        with pytest.raises(TDQuoteException) as excinfo:
+            imds_client.get_td_quote('some_encoded_report')
+        assert 'Error decoding JSON response. Error' in str(excinfo.value)
+    assert mock_post.call_count == 1
+
+
+def test_vcek_cert_fails_to_decode_json_response(mocker, imds_client):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.text = 'invalid_json'
+
+    with patch('requests.get', return_value=mock_response) as mock_post:
+        with pytest.raises(VcekCertException) as excinfo:
+            imds_client.get_vcek_certificate()
+        assert 'Error decoding JSON response. Error' in str(excinfo.value)
+    assert mock_post.call_count == 1
