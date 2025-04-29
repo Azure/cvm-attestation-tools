14d0dcd04232ea28880fb8f3fb6f673140ed7769
Javier <jave@microsoft.com>
2025-04-28 17:51:57 -0700

    removing print statement

--- cvm-attestation/AttestationClient.py
+++ cvm-attestation/AttestationClient.py
@@ -140,15 +140,14 @@ class AttestationClient():
       if report_type == 'snp' and isolation_type == IsolationType.SEV_SNP:
         self.log_snp_report(hw_report)
       elif report_type == 'tdx' and isolation_type == IsolationType.TDX:
-        # self.log.info("Fetching td quote...")
+        self.log.info("Fetching td quote...")
         imds_client = ImdsClient(self.log)
         encoded_report = Encoder.base64url_encode(hw_report)
         encoded_hw_evidence = imds_client.get_td_quote(encoded_report)
         hw_report = Encoder.base64url_decode(encoded_hw_evidence)
-        print(hw_report, report_type, runtime_data)
-        # self.log.info("Finished fetching td quote")
+        self.log.info("Finished fetching td quote")
 
-        # self.log.info("Hardware report parsing for TDX not supported yet")
+        self.log.info("Hardware report parsing for TDX not supported yet")
       else:
         raise UnsupportedReportTypeException(f"Unsupported report type: {report_type}")
 
